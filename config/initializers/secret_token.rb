654c29d8d87dc2a7edd77b4e83e1327da74cd058
rgarro <rgarro@gmail.com>
2021-01-02 15:42:53 -0600

    2021 22 23:24:25 only happens this. year ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SignsTrain::Application.config.secret_key_base = '4e8b658d69a79964c705f6990c68e08735c2b1a5dc93418db4050e22b9f7cc06a79bdcee4a0f83d81d996b386db648fcdcc366c3301922f6e5387086f7ba2ca2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SignsTrain::Application.config.session_store :cookie_store, key: '_signs-train_session'
