7275a61a3b0955473090687ad3a3f56ed61cdb1b
Rolando Garro <rgarro@gmail.com>
2021-01-25 09:45:34 -0600

    zipping chai where virilla and tibas joint ...

--- test/models/login_test.rb
+++ test/models/login_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
+#the truth is the last output at the end of the times ....
+#this will be the the OrangeSession Gem at 9:23pm someday
+# secure login at santa elena crossroad stacking evergreen container
+# on a plsql moment ...
+# Kowalsky did you hear this. ?
+
 class LoginTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "assert it has do_login method" do
+      authObj= Login.new
+      assert(authObj.class.method_defined?(:do_login), "knock knock ...")
+  end
+
+  test "assert it has create_login_session method" do
+      #wiil be back at 4:20 ...
+  end
+
+   test "the truth" do
+     assert true
+   end
+
 end
